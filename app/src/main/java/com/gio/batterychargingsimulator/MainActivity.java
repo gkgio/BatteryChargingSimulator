1e8929315598b410dad51bc7f3b72938e018e96e
Georgy <gio.0000@mail.ru>
2017-04-16 13:06:40 +0300

    Реализована симуляция индикатора заряда батарей
    Реализация разукрашенной фигуры с чёрной обводкой
         и слегка скругленными краями с фигурой повер.
    Также закоменчена реализация только с фигурой

--- app/src/main/java/com/gio/batterychargingsimulator/MainActivity.java
+++ app/src/main/java/com/gio/batterychargingsimulator/MainActivity.java
@@ -1,13 +1,50 @@
 package com.gio.batterychargingsimulator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View viewBattery;
+
+    // Аниматор фона
+    private ValueAnimator mAnimator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(R.string.main_page_title);
+
+        viewBattery = findViewById(R.id.viewBattery);
+
+        // Создаём аниматор
+        mAnimator = ValueAnimator.ofInt(0, 10000);
+        mAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        mAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        mAnimator.setDuration(3000);
+        mAnimator.setInterpolator(new DecelerateInterpolator());
+        mAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                Integer value = (Integer) valueAnimator.getAnimatedValue();
+                viewBattery.getBackground().setLevel(value);
+            }
+        });
+        mAnimator.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mAnimator.cancel();
+        mAnimator.removeAllUpdateListeners();
+        mAnimator = null;
     }
 }
+
+
